aefb8e64df758e8e9778f32ef3d42eb4b6142576
petermordaunt <peetfire@hotmail.com>
2022-01-06 13:56:40 +0000

    film by year - no films test passed

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -50,7 +50,11 @@ describe('Cinema', function () {
     const expected = [bladeRunner, dunkirk, trainspotting];
     assert.deepStrictEqual(actual, expected);
   });
-  it('should be able to check whether there are no films from a particular year');
+  it('should be able to check whether there are no films from a particular year', function(){
+    const actual = cinema.getFilmsByYear(2022);
+    const expected = [];
+    assert.deepStrictEqual(actual, expected);
+  });
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
 
