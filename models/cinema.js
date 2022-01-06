0f023758b358e82f78f0b1f856ece391f94c09c5
petermordaunt <peetfire@hotmail.com>
2022-01-06 13:39:26 +0000

    Setup and film list test passed

---
+++ models/cinema.js
@@ -0,0 +1,9 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.getFilmTitles= function(){
+  return this.films.map(film => film.title);
+}
+
+module.exports = Cinema;
